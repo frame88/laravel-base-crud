43034fdd9223a330a7ec4c9c43c8eda856ae67cf
frame88 <fleuzzi97@gmail.com>
2022-02-23 17:55:30 +0100

    scaff + traccia

---
+++ laravel-base-crud/resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+{{-- 
+mercoled% 23 febb
+Ciao ragazzi,
+Esercizio di oggi:
+nome repo: laravel-base-crud
+oggi create un nuovo progetto Laravel 7 e tramite gli appositi comandi artisan create un model con relativa migration e un resource controller.
+Iniziate a definire le operazioni CRUD per gestire un archivio di fumetti.
+Partite da create, store e show, solo dopo la index
+Utilizzate Bootstrap
+Repo di oggi:
+https://github.com/ChiaraPassaro/classe49-base-crud --}}
+</body>
+</html>
